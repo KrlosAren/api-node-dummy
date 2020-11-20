21aa9fc7f34bfe7d918d48e25ad2fdee1ccd2aff
KrlosAren <krlosaren@gmail.com>
2020-11-20 19:26:26 -0300

    fixed name of user.my-sql

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ require('dotenv').config({ debug: process.env.DEBUG })
 const config = {
   dev: process.env.NODE_ENV !== 'production',
   host: process.env.HOST,
-  user: process.env.USER,
+  user: process.env.USER_MYSQL,
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
   port: process.env.PORT || 3000,
