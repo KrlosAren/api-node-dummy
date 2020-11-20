35fdae9677d6f71668a2ef03c504cee3d3c07b38
KrlosAren <krlosaren@gmail.com>
2020-11-20 07:32:27 -0300

    changes in index file.

--- src/config/index.js
+++ src/config/index.js
@@ -3,8 +3,7 @@ require('dotenv').config({ debug: process.env.DEBUG })
 const config = {
   dev: process.env.NODE_ENV !== 'production',
   host: process.env.HOST,
-  // user: process.env.USER ,
-  user: 'b03e909fe7dc5a' ,
+  user: process.env.USER,
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
   port: process.env.PORT || 3000,
